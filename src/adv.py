c90ca284f02fd3be5008304d326aa2f431d130e1
littleonetwo <littleonetwo@gmail.com>
2020-08-13 20:42:39 -0500

    mvp complete

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,34 @@
 from room import Room
 from player import Player
+from item import Item
+import os
+from time import sleep
+
+
+items = {
+  'coin': Item("coin", "A small shimmering golden [coin]"),
+  'sword': Item("sword", "A worn [sword] used extensively in what was perhaps an exhausting battle")
+}
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", 'outside'),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",'foyer'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",'overlook'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",'narrow'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",'treasure'),
 }
 
 
@@ -34,6 +43,9 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+
+room['treasure'].items.append(items['coin'])
+room['outside'].items.append(items['sword'])
 #
 # Main
 #
@@ -52,13 +64,30 @@ room['treasure'].s_to = room['narrow']
 # If the user enters "q", quit the game.
 
 user_input = 0
-player = Player("Guy",room['outside'] )
+
+user_input = input("A deep voice comes over your senses, \nHELLO GOOD HUMAN!\nWhat is YOUR Name?: ")
+
+player = Player(user_input, room['outside'] )
 prev_dir = ""
 
+os.system('clear')
 
-def update_game(new_room, prev_direction, item = "nothing" ): #function to call whenever a valid action takes place
+sleep(1)
+
+print(f"Welcome {player.name}! I will now send you to a land full of treasures, it is your duty to find them and to also return!")
+
+sleep(7)
+
+os.system('clear')
+print("YOUR BODY IS DISINIGRATED LIKE ASH IN THE WIND, AFTER A FEW SECONDS YOUR BODY MATERIALIZES IN FRONT OF AN OMINOUS CAVE.")
+
+sleep(7)
+
+os.system('clear')
+
+def update_game( prev_direction, item = "nothing" ): #function to call whenever a valid action takes place
   player.prev_room = prev_direction
-  player.cur_room = new_room
+
 
   if item != "nothing":
     player.inventory_cap += 1
@@ -66,16 +95,73 @@ def update_game(new_room, prev_direction, item = "nothing" ): #function to call 
 
 
 
+def validate_direction(cur_room, direction):
+  if " " in direction:
+    word1 = direction[0:direction.find(" ")]
+    word2 = direction[(direction.find(" ")+1):]
 
+    if word1 == "get" or word1 == "take":
+      item = cur_room.get_item(word2)
+      if item:
+        item.on_take()
+        cur_room.remove_item(item)
+        player.inventory_list.append(item)
+      else:
+        print(f"{word2} does not exist in this room")
+    elif word1 =="drop":
+      item = player.check_item(word2)
+      if item:
+        cur_room.items.append(item)
+        player.remove_item(item)
+        print(f"You dropped your {item.name}")
+      else:
+        print(f"{word2} does not exist in your bag.")
+
+
+
+  if direction == "n":
+    if hasattr(cur_room, "n_to"):
+      player.cur_room = room[cur_room.n_to.key]
+      return
+
+  elif direction == "s":
+    if hasattr(cur_room, "s_to"):
+      player.cur_room = room[cur_room.s_to.key]
+      return
+
+  elif direction == "e":
+    if hasattr(cur_room, "e_to"):
+      player.cur_room = room[cur_room.e_to.key]
+      return
+
+  elif direction == "w":
+    if hasattr(cur_room, "w_to"):
+      player.cur_room = room[cur_room.w_to.key]
+      return
+  #this will only run if there was an incorrect direction
+  return "You cannot do that at this time."
+
+
+
+
+print("Name: "+ player.name)
+i = 0
 while user_input != "q":
-  print("Name: "+ player.name)
+
   print("Current Room: " + player.cur_room.name)
-  print("\nRoom description: " + player.cur_room.description)
+  if len(player.cur_room.items) == 0:
+    print("\nRoom description: " + player.cur_room.description)
+  else:
+    print("\nRoom description: " + player.cur_room.description +" You see:")
+    while i < len(player.cur_room.items):
+      print(player.cur_room.items[i].description)
+      i+=1
+    i=0
   print("\nYou came from the " + player.prev_room + " direction.")
 
-  user_input = input("Choose a Direction: [n]orth, [s]outh, [e]ast, [w]est, or [q]uit: ")
+  user_input = input("Choose an Action: [n]orth, [s]outh, [e]ast, [w]est, [get][item], [drop][item], or [q]uit: ")
 
-  validate_move = player.validate_direction(player.cur_room.name, user_input)
+  validate_move = validate_direction(player.cur_room, user_input)
 
 
 
@@ -89,7 +175,7 @@ while user_input != "q":
     else:
       prev_dir = "West"
 
-    update_game(room[validate_move], prev_dir )
+    update_game(prev_dir )
 
   else:
     if user_input != "q":
--- src/player.py
+++ src/player.py
@@ -1,44 +1,28 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
 
 class Player:
 
     def __init__(self, name, cur_room, prev_room = "No Where", inventory_cap = 0, inventory_list = []):
         self.name = name
-        self.cur_room = cur_room
+        self.cur_room: Room = cur_room
         self.prev_room = prev_room
         self.inventory_cap = inventory_cap
         self.inventory_list= inventory_list
 
+
+    def check_item(self, item_name: str):
+      for items in self.inventory_list:
+        if items.name.lower() == item_name.lower():
+          return items
+
+        return None
+
+    def remove_item(self, item_name):
+      self.inventory_list.remove(item_name)
+
+
     def __str__(self):
         return f"Name = {self.name}, Current Room = {self.cur_room},Previous Room = {self.prev_room}, Inventory Count = {self.inventory_cap}, Inventory Items = {self.inventory_list}"
-
-    def validate_direction(self, cur_room, direction):
-      if cur_room == "Outside Cave Entrance":
-        if direction == "n":
-          return "foyer"
-
-      elif cur_room == "Foyer":
-        if direction == "s":
-          return "outside"
-        elif direction == "n":
-          return "overlook"
-        elif direction == "e":
-          return "narrow"
-
-      elif cur_room =="Grand Overlook":
-        if direction == "s":
-          return "foyer"
-
-      elif cur_room == "Narrow Passage":
-        if direction == "n":
-          return "treasure"
-        elif direction == "w":
-          return "foyer"
-
-      elif cur_room =="Treasure Chamber":
-        if direction == "s":
-          return "narrow"
-
-
-      return "You cannot go that direction at this time."
